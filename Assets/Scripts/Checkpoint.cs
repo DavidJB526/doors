e6c4f0d13c7e1cb871f2f737be802f7f0301c191
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-11-12 16:59:59 -0600

    Add Project

---
+++ Assets/Scripts/Checkpoint.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Checkpoint : MonoBehaviour {
+
+    [SerializeField]
+    private float inactiveRotationSpeed = 100f, activateRotationSpeed = 300f;
+
+    [SerializeField]
+    private float inactiveScale = 1f, activeScale = 1.5f;
+
+    [SerializeField]
+    private Color inactiveColor, activeColor;
+
+    private bool isActivated = false;
+    private SpriteRenderer spriteRenderer;
+
+    private void Start()
+    {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        UpdateColor();
+    }
+
+    private void Update()
+    {
+        UpdateRotation();
+    }
+
+    private void UpdateColor()
+    {
+        Color color = inactiveColor;
+
+        if (isActivated)
+        {
+            color = activeColor;
+        }
+
+        spriteRenderer.color = color;
+    }
+
+    private void UpdateScale()
+    {
+        float scale = inactiveScale;
+        if (isActivated)
+        {
+            scale = activeScale;
+        }
+        transform.localScale = Vector3.one * scale;
+    }
+
+    private void UpdateRotation()
+    {
+        float rotationSpeed = inactiveRotationSpeed;
+        if (isActivated)
+        {
+            rotationSpeed = activateRotationSpeed;
+        }
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
+    }
+    
+    public void SetIsActivated(bool value)
+    {
+        isActivated = value;
+        UpdateScale();
+        UpdateColor();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            Debug.Log("Player entered checkpoint");
+            PlayerController player = collision.GetComponent<PlayerController>();
+            player.SetCurrentCheckpoint(this);
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    private float maxSpeed, jumpForceUp;
+    [SerializeField]
+    private Collider2D groundDetectTrigger;
+    [SerializeField]
+    private ContactFilter2D groundContactFilter;
+
+
+    private Rigidbody2D rb2d;
+    private Checkpoint currentCheckpoint;
+    private Collider2D[] groundHitDetectionResults = new Collider2D[16];
+    private bool grounded;
+    private bool isDead = false;
+    private bool isFacingRight = true;
+
+    private void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        isFacingRight = true;
+        isDead = false;
+    }
+
+    private void Update()
+    {
+        if (!isDead)
+        {
+            GroundCheck();
+        }        
+    }
+
+    private void FixedUpdate()
+    {
+        if (!isDead)
+        {
+            Move();
+            Jump();
+        }                                   
+    }
+
+    private void Move()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, 0.0f);
+
+        rb2d.AddForce(Vector2.right * movement, ForceMode2D.Impulse);
+        rb2d.velocity = Vector2.ClampMagnitude(rb2d.velocity, maxSpeed);
+    }
+
+    private void Jump()
+    {
+
+        if (Input.GetButtonDown("Jump") && grounded && !isDead)
+        {
+            rb2d.AddForce(Vector2.up * jumpForceUp, ForceMode2D.Impulse);
+        }
+    }
+
+    private void GroundCheck()
+    {
+        grounded = groundDetectTrigger.OverlapCollider(groundContactFilter, groundHitDetectionResults) > 0;
+    }
+
+    private void Flip()
+    {
+        isFacingRight = !isFacingRight;
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
+
+    public void SetCurrentCheckpoint(Checkpoint newCurrentCheckpoint)
+    {
+        if (currentCheckpoint != null)
+        {
+            currentCheckpoint.SetIsActivated(false);            
+        }
+
+        currentCheckpoint = newCurrentCheckpoint;
+        currentCheckpoint.SetIsActivated(true);
+    }
+
+    public void Respawn()
+    {
+        if (currentCheckpoint == null)
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        }
+        else
+        {
+            rb2d.velocity = Vector2.zero;
+            transform.position = currentCheckpoint.transform.position;
+        }        
+    }
+}
