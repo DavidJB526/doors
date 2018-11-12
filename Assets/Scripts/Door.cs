4802acb853e71dcc36fd59020102da481214cbe8
DavidJB526 <david.beaudoin@loop.colum.edu>
2018-11-12 17:27:01 -0600

    Finish Tutorial

--- Assets/Scripts/Door.cs
+++ Assets/Scripts/Door.cs
@@ -1,17 +1,47 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Door : MonoBehaviour {
 
-    private void OnTriggerStay2D(Collider2D collision)
+    [SerializeField]
+    private string sceneToLoad;
+
+    private bool isPlayerInTrigger;
+
+    //private void OnTriggerStay2D(Collider2D collision)
+    //{
+        //if (collision.CompareTag("Player"))
+        //{            
+        //    if (Input.GetButtonDown("Activate"))
+        //    {
+        //        Debug.Log("Player activated door");
+        //    }
+        //}
+    //}
+
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
-        {            
-            if (Input.GetButtonDown("Activate"))
-            {
-                Debug.Log("Player activated door");
-            }
+        {
+            isPlayerInTrigger = true;
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            isPlayerInTrigger = false;
+        }
+    }
+
+    private void Update()
+    {
+        if (Input.GetButtonDown("Activate") && isPlayerInTrigger)
+        {
+            SceneManager.LoadScene(sceneToLoad);
         }
     }
 }
